2210a2403f0abb681badee9d95d2e33285217a13
jrdAlexandr <tezkaka0711@gmail.com>
2020-11-18 19:21:10 +0200

    textarea => span

--- telegrammdex/src/components/ChatInput/index.jsx
+++ telegrammdex/src/components/ChatInput/index.jsx
@@ -1,28 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './ChatInput.scss';
 import iconAddFile from '../../assets/images/icon-add-file.svg';
 import iconSendMessage from '../../assets/images/icon-send-message.svg';
 const ChatInput = () => {
+const [inputMessage, setInputMessage] = useState('Write a message...')
+  const inputMessageRef = useRef()
   const [rowsText, setRowsText] = useState(1);
+  // const handlerInputMessage = (e) => {
+  //   console.log(getComputedStyle(e.target).height);
+  //   if ((e.target.scrollHeight - 4) / 21 < 7) {
+  //     setRowsText((e.target.scrollHeight - 4) / 21);
+  //   }
+  // };
   const handlerInputMessage = (e) => {
-    console.log(getComputedStyle(e.target).height);
-    if ((e.target.scrollHeight - 4) / 21 < 7) {
-      setRowsText((e.target.scrollHeight - 4) / 21);
-    }
+    console.log(e.target);
+    
   };
+
+
   const handlerSendMessage = () => {
-    setRowsText(1);
+   
+    console.log( inputMessageRef.current.innerText)
+
+    // setRowsText(1);
   };
 
   return (
     <div className='chat-input'>
       <img className='chat-input__icon' src={iconAddFile} alt='icon add file' />
-      <textarea
+      {/* <textarea
         onChange={handlerInputMessage}
-        className='chat-input__input'
+       c
         placeholder='Write a message...'
         rows={rowsText}
-      ></textarea>
+      ></textarea> */}
+      <span  className='chat-input__input' ref={inputMessageRef} onClick={()=>{setInputMessage('')}}  role="textbox" contenteditable="true">{inputMessage}</span>
       <img
         className='chat-input__icon'
         src={iconSendMessage}
