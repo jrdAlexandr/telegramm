58069e2fee88a23e9a3b1fd23a2e1c3a8f2b3aea
jrdAlexandr <tezkaka0711@gmail.com>
2020-11-18 17:06:16 +0200

    page

--- telegrammdex/src/App.js
+++ telegrammdex/src/App.js
@@ -1,12 +1,33 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import Dialogs from './components/Dialogs';
+import EmptyPage from './components/EmptyPage';
 import Messages from './components/Messages';
 function App() {
   return (
-    <div className='App'>
-      <Dialogs />
-      <Messages />
-    </div>
+    <Router>
+      <div className='App'>
+        <Switch>
+          {/* <Route exact path='/'>
+            <Dialogs />
+            <EmptyPage />
+          </Route> */}
+
+          <Route exact path='/'>
+            <Dialogs />
+            <Messages />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+    // <div className='App'>
+    //   <Dialogs />
+
+    //   <EmptyPage />
+
+    //   <Messages />
+    // </div>
   );
 }
 
---
+++ telegrammdex/src/components/EmptyPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './EmptyPage.scss'
+const EmptyPage = () => {
+  return (
+    <div className='empty-page'>
+      <p>Please select a chat to start messaging</p>
+    </div>
+  );
+};
+export default EmptyPage;
