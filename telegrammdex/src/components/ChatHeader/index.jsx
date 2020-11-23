1a96d4d65c96113af2549143998e9314b94e386a
jrdAlexandr <tezkaka0711@gmail.com>
2020-11-23 09:20:18 +0200

    add button home

--- telegrammdex/src/components/ChatHeader/index.jsx
+++ telegrammdex/src/components/ChatHeader/index.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './ChatHeader.scss';
 import iconChatInfo from '../../assets/images/icon-chat-info.svg';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getMessages } from '../../redux/actions/messages/messages';
+import iconLinkHome from '../../assets/images/icon-link-home.svg';
 const ChatHeader = ({ dialogTitle }) => {
   const [chatTitle, setChatTitle] = useState('');
   const dispatch = useDispatch();
   const dialogs = useSelector((state) => state.dialogs.dialogs);
   const { chat } = useParams();
-  // useEffect(() => {
-    
-  // }, []);
+
   useEffect(() => {
     dialogs?.map((dialog) =>
       dialog.id == chat ? setChatTitle(dialog.title) : null
@@ -21,10 +20,24 @@ const ChatHeader = ({ dialogTitle }) => {
 
   return (
     <div className='chat-header'>
+  
+      
       <div className='chat-header__interlocutor'>
-        <div className='chat-header__interlocutor__name'>{chatTitle}</div>
-        <div className='chat-header__interlocutor__time'>
+      <Link to='/'>
+      <button className='chat-header__interlocutor__link' type='button'>
+        <img
+          src={iconLinkHome}
+          alt='icon link home'
+          
+        />
+      </button>
+      </Link>
+      <div className="chat-header__interlocutor__info">
+       <div className='chat-header__interlocutor__info__name'>{chatTitle}</div>
+        <div className='chat-header__interlocutor__info__time'>
           last seen 3 hours ago
+        </div>
+       
         </div>
       </div>
       <div className='chat-header__btn-info'>
